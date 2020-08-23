a6123b33d84da8b9ccef87719893487ad1aaee52
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2020-08-23 12:36:38 +0530

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -10,20 +10,20 @@ class Paper
 
 			
 			}
-			this.image=loadImage("paper.png");
+			
 		this.x=x;
 		this.y=y;
 		this.r=r;
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options);
-		
+		this.image=loadImage("paper.png");
 
 		World.add(world, this.body);
 		
 	}
 	display()
-	{
-		  imageMode(CENTER)
-		    image(this.image,this.body.position.x,this.body.position.y,70,70);
+	{       
+		imageMode(CENTER)
+		image(this.image,this.body.position.x,this.body.position.y,70,70);
 			var paperpos=this.body.position;		
 
 			push()
